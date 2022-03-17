dc10cf1279e25132a12e7a4a3a2eee51d94bb42c
johanz9 <elmejorz98@hotmail.com>
2022-03-17 17:38:05 +0100

    Update __manifest__.py

--- extra-addons/estate/__manifest__.py
+++ extra-addons/estate/__manifest__.py
@@ -5,7 +5,7 @@
     'name': 'ESTATE',
     'version': '1.0',
     'category': 'Sales/ESTATE',
-    'sequence': -100,
+    'sequence': 2,
     'summary': 'estate management',
     'description': "",
     'website': 'https://www.odoo.com/page/estate',
@@ -14,16 +14,19 @@
         'base_setup',
     ],
     'data': [
-        'security/ir.model.access.csv',
         'security/security.xml',
-        'views/estate_menu_views.xml',
+        'security/ir.model.access.csv',
+
         'views/estate_property_view.xml',
         'views/estate_property_type_view.xml',
+        'views/estate_menu_views.xml',
+
         'views/res_users_views.xml',
         'views/css_loader.xml',
-        'demo/estate_demo.xml',
     ],
-    'demo': [],
+    'demo': [
+        'data/estate_demo.xml',
+    ],
     'qweb': [],
     'css': ['static/src/css/crm.css'],
     'application': True,
